44a1544d712cc0c9d10dc2ad4ae57f8378338c82
ria147 <62183694+ria147@users.noreply.github.com>
2024-10-13 21:17:49 -0400

    define listing table in the database

--- backend/apps/listings/models.py
+++ backend/apps/listings/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Listing(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    condition = models.CharField(max_length=50)
+    category = models.CharField(max_length=50)
+    pickup_location = models.CharField(max_length=100)
+    photo = models.URLField()
+
+    def __str__(self):
+        return self.name
