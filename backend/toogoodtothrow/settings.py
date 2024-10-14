7f538c513101c95eaf7b730b2588e88a69d8bcc0
ria147 <62183694+ria147@users.noreply.github.com>
2024-10-13 20:32:46 -0400

    init for new app called listings

--- backend/toogoodtothrow/settings.py
+++ backend/toogoodtothrow/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'apps.products'
+    'apps.products',
+    'apps.listings'
 ]
 
 MIDDLEWARE = [
