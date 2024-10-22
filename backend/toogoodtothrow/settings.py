6dc60af34d2d51b9189cbecfe8fbf5be759d9beb
amyy2 <27amys@gmail.com>
2024-10-22 13:39:29 -0400

    Migrate redis server to AWS

--- backend/toogoodtothrow/settings.py
+++ backend/toogoodtothrow/settings.py
@@ -175,7 +175,8 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [("127.0.0.1", 6379)],
+            #"hosts": [("127.0.0.1", 6379)],
+            'hosts': [('redis-cache-444-y0uvf8.serverless.use1.cache.amazonaws.com:6379', 6379)],
         },
     },
 }
