d5e24524a389ca308a9698f89fc4b5e202b2fd01
amyy2 <27amys@gmail.com>
2024-10-24 09:36:46 -0400

    Add CSS to product grid

--- frontend/src/components/screens/PrivateMessaging.js
+++ frontend/src/components/screens/PrivateMessaging.js
@@ -274,7 +274,7 @@ function PrivateMessage() {
                     <div key={conversation.id} onClick={() => setSelectedConversationId(conversation.id)}
                         className={conversation.id === selectedConversationId ? 'selected' : '' || !conversation.is_read ? 'unread' : ''}>
                         <span className={!conversation.is_read ? 'dot' : ''}></span>
-                        <span className="title">
+                        <span className="message-title">
                             {conversation.name}
                             <p>{conversation.last_sender_id == currentUser.id ? 'You' : conversation.last_sender_name}: {conversation.last_message}</p>
                         </span>
--- frontend/src/components/screens/Products.js
+++ frontend/src/components/screens/Products.js
@@ -1,9 +1,22 @@
 import React, { useState, useEffect } from "react";
 import './Products.css';
+import { Select, MenuItem, FormControl, InputLabel } from '@mui/material';
 
 const Products = () => {
   const [products, setProducts] = useState([]);
+  const [sortOption, setSortOption] = useState('');
   const token = localStorage.getItem('authToken');
+
+  const sortedProducts = [...products].sort((a, b) => {
+    if (sortOption === 'priceAsc') {
+      return a.price - b.price;  // Sort by price ascending
+    } else if (sortOption === 'priceDesc') {
+      return b.price - a.price;  // Sort by price descending
+    } else if (sortOption === 'nameAsc') {
+      return a.name.localeCompare(b.name);  // Sort by name alphabetically
+    }
+    return 0; // Default (no sorting)
+  });
 
   useEffect(() => {
     fetch('http://3.87.240.14:8000/api/products/', {
@@ -21,15 +34,37 @@ const Products = () => {
         <div className="filters">
           filters
         </div>
-        <div className="products">
         <div className="product-grid">
-          {products.map(product => (
-            <div key={product.id} className="product-item">
-              <h3>{product.name}</h3>
-              <p>${product.price}</p>
-            </div>
-          ))}
-        </div>
+
+          <div className="title">
+            <h1>Featured listings</h1>
+            <FormControl className="sort-by" variant="outlined" style={{ minWidth: 200, marginBottom: '20px' }}>
+              <InputLabel id="sort-label">Sort By</InputLabel>
+              <Select
+                labelId="sort-label"
+                value={sortOption}
+                onChange={(e) => setSortOption(e.target.value)}
+                label="Sort By"
+              >
+                <MenuItem value="priceAsc">Price: Low to High</MenuItem>
+                <MenuItem value="priceDesc">Price: High to Low</MenuItem>
+                <MenuItem value="nameAsc">Name: A-Z</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+
+          <div className="products">
+            {sortedProducts.map(product => (
+              <div key={product.id} className="product-item">
+                <span className="product-image"></span>
+                <div className="product-text">
+                  <div className="product-price">${product.price}</div>
+                  <div className="product-title">{product.name}</div>
+                  <div className="product-location">{product.pickup_location}</div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
     </div>
   );
