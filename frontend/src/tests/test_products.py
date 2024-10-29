9bc5a60307292f229b428591b1332ea1b5a02dfa
ria147 <62183694+ria147@users.noreply.github.com>
2024-10-29 02:05:13 -0400

    Test for multiple filters combined and clearing all filters - frontend

--- frontend/src/tests/test_products.py
+++ frontend/src/tests/test_products.py
@@ -23,6 +23,7 @@ PRODUCTS_CONTAINER = '.products-container'
 PRODUCT_GRID = '.products'
 PRODUCT_ITEM = '.product-item'
 PRICE_SLIDER_TRACK = ".MuiSlider-track"
+CLEAR_FILTERS = 'Clear Filters'
 
 ROLE_BUTTON = 'button'
 ROLE_OPTION = 'option'
@@ -106,7 +107,6 @@ def test_filter_by_category(page):
     for choice in CATEGORY_CHOICES:
         # Click on the Category label or dropdown
         page.get_by_label(CATEGORY).click()
-        page.wait_for_timeout(WAIT_TO_LOAD_SHORT)
         
         # Select the option with the name 'Textbook'
         page.get_by_role(ROLE_OPTION, name=choice).click()
@@ -138,7 +138,6 @@ def test_filter_by_condition(page):
     for choice in CONDITION_CHOICES:
         # Click on the Condition label or dropdown
         page.get_by_label(CONDITION).click()
-        page.wait_for_timeout(WAIT_TO_LOAD_SHORT)
         
         # Select the option with the name 'Textbook'
         page.get_by_role(ROLE_OPTION, name=choice, exact=True).click()
@@ -170,7 +169,6 @@ def test_filter_by_location(page):
     for choice in LOCATION_CHOICES:
         # Click on the Condition label or dropdown
         page.get_by_label(LOCATION).click()
-        page.wait_for_timeout(WAIT_TO_LOAD_SHORT)
         
         # Select the option with the name 'Textbook'
         page.get_by_role(ROLE_OPTION, name=choice).click()
@@ -234,7 +232,7 @@ def test_price_slider_random(page):
     random_number = random.randint(1, 100)
     random_width_percentage = random_number / 100
 
-    # Slider will be dragged halfway for this test
+    # Slider will be dragged to a random position for this test
     target_x = price_slider_position['x'] + price_slider_position['width'] * random_width_percentage
     target_y = price_slider_position['y'] + price_slider_position['height'] / 2 
     page.mouse.click(target_x, target_y)
@@ -245,3 +243,114 @@ def test_price_slider_random(page):
     print("..Slider is dragged to", random_number, "% width -- Filtered count:", filtered_product_count)
     assert filtered_product_count <= original_product_count
     print("Test: Slider can be dragged to any length of the slider generated randomly")
+
+
+def test_all_filter_combinations(page):
+    """ Test to ensure all filters can be combined together"""
+    page.goto(PRODUCTS_PAGE_URL)
+    page.wait_for_timeout(WAIT_TO_LOAD_LONG) 
+    assert page.locator(PRODUCTS_CONTAINER).is_visible()
+
+    # Count original products
+    original_product_count = page.locator(PRODUCT_GRID).locator(PRODUCT_ITEM).count() # count original products listed in the grid
+    print(".Original count:", original_product_count)
+
+    # Generate a random category, condition, location and random percentage where we want to drag the slider
+    random_category_index = random.randint(0, len(CATEGORY_CHOICES))
+    random_condtion_index = random.randint(0, len(CONDITION_CHOICES))
+    random_location_index = random.randint(0, len(LOCATION_CHOICES))
+    random_number_slider = random.randint(1, 100)
+    random_width_percentage = random_number_slider / 100
+
+    # Click on the Category chosen randomly
+    page.get_by_label(CATEGORY).click()
+    page.get_by_role(ROLE_OPTION, name=CATEGORY_CHOICES[random_category_index]).click()
+    page.wait_for_timeout(WAIT_TO_LOAD_SHORT)
+
+    # Count number of products returned after single filter
+    filtered_product_count_1_filter = page.locator(PRODUCT_GRID).locator(PRODUCT_ITEM).count() # count filtered products listed in the grid
+    print("..After category filtering -- Filtered count:", filtered_product_count_1_filter)
+    assert filtered_product_count_1_filter <= original_product_count
+
+    # Click on the Condition chosen randomly
+    page.get_by_label(CONDITION).click()
+    page.get_by_role(ROLE_OPTION, name=CONDITION_CHOICES[random_condtion_index]).click()
+    page.wait_for_timeout(WAIT_TO_LOAD_SHORT)
+
+    # Count number of products returned after two filters
+    filtered_product_count_2_filter = page.locator(PRODUCT_GRID).locator(PRODUCT_ITEM).count() # count filtered products listed in the grid
+    print("..After category & condition filtering -- Filtered count:", filtered_product_count_2_filter)
+    assert filtered_product_count_2_filter <= filtered_product_count_1_filter
+
+    # Click on the Location chosen randomly
+    page.get_by_label(LOCATION).click()
+    page.get_by_role(ROLE_OPTION, name=LOCATION_CHOICES[random_location_index]).click()
+    page.wait_for_timeout(WAIT_TO_LOAD_SHORT)
+
+    # Count number of products returned after three filters
+    filtered_product_count_3_filter = page.locator(PRODUCT_GRID).locator(PRODUCT_ITEM).count() # count filtered products listed in the grid
+    print("..After category, condition & location filtering -- Filtered count:", filtered_product_count_3_filter)
+    assert filtered_product_count_3_filter <= filtered_product_count_2_filter
+
+    # Drag slider to the chosen posiion
+    price_slider = page.locator(PRICE_SLIDER_TRACK)
+    price_slider_position = price_slider.bounding_box()
+    target_x = price_slider_position['x'] + price_slider_position['width'] * random_width_percentage
+    target_y = price_slider_position['y'] + price_slider_position['height'] / 2 
+    page.mouse.click(target_x, target_y)
+    page.wait_for_timeout(WAIT_TO_LOAD_SHORT)
+
+    # Count number of products returned after four filters
+    filtered_product_count_4_filter = page.locator(PRODUCT_GRID).locator(PRODUCT_ITEM).count() # count filtered products listed in the grid
+    print("..After category, condition, locaion & price filtering -- Filtered count:", filtered_product_count_4_filter)
+    assert filtered_product_count_4_filter <= filtered_product_count_3_filter
+    print("Test: Multiple filters can be combined together")
+
+
+def test_clear_all_filters(page):
+    """ Test to ensure all filters can be cleared after multiple filters are applied """
+    page.goto(PRODUCTS_PAGE_URL)
+    page.wait_for_timeout(WAIT_TO_LOAD_LONG) 
+    assert page.locator(PRODUCTS_CONTAINER).is_visible()
+
+    # Count original products
+    original_product_count = page.locator(PRODUCT_GRID).locator(PRODUCT_ITEM).count() # count original products listed in the grid
+    print(".Original count:", original_product_count)
+
+    # Generate a random category, condition, location and random percentage where we want to drag the slider
+    random_category_index = random.randint(0, len(CATEGORY_CHOICES))
+    random_condtion_index = random.randint(0, len(CONDITION_CHOICES))
+    random_location_index = random.randint(0, len(LOCATION_CHOICES))
+    random_number_slider = random.randint(1, 100)
+    random_width_percentage = random_number_slider / 100
+    
+    # Set up a random combination of filters
+    page.get_by_label(CATEGORY).click()
+    page.get_by_role(ROLE_OPTION, name=CATEGORY_CHOICES[random_category_index]).click()  # Choose a random category
+    page.get_by_label(CONDITION).click()
+    page.get_by_role(ROLE_OPTION, name=CONDITION_CHOICES[random_condtion_index]).click()  # Choose a random condition
+    page.get_by_label(LOCATION).click()
+    page.get_by_role(ROLE_OPTION, name=LOCATION_CHOICES[random_location_index]).click()  # Choose a random location
+    page.wait_for_timeout(WAIT_TO_LOAD_SHORT)
+
+    # Set up the price slider
+    price_slider = page.locator(PRICE_SLIDER_TRACK)
+    price_slider_position = price_slider.bounding_box()
+    target_x = price_slider_position['x'] + price_slider_position['width'] * random_width_percentage
+    target_y = price_slider_position['y'] + price_slider_position['height'] / 2 
+    page.mouse.click(target_x, target_y)
+    page.wait_for_timeout(WAIT_TO_LOAD_SHORT)
+
+    # Count number of products returned after single filter
+    filtered_product_count = page.locator(PRODUCT_GRID).locator(PRODUCT_ITEM).count() # count filtered products listed in the grid
+    print("..After all filtering -- Filtered count:", filtered_product_count)
+    assert filtered_product_count <= original_product_count
+
+    # Locate "Clear Filters" button and click
+    page.get_by_role(ROLE_BUTTON, name=CLEAR_FILTERS).click()
+    unfiltered_product_count = page.locator(PRODUCT_GRID).locator(PRODUCT_ITEM).count() # count filtered products listed in the grid
+    print("..After clearing all filtering -- Product Count:", unfiltered_product_count)
+    assert unfiltered_product_count == original_product_count
+    print("Test: All filters can be cleared")
+
+
