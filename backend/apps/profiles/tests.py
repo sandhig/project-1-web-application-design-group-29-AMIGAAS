86b3d6e2af1fa8e81a64028c3262bf034c75811b
ria147 <62183694+ria147@users.noreply.github.com>
2024-10-31 21:31:37 -0400

    Update tests for newer model, new tests for verification code generation

--- backend/apps/profiles/tests.py
+++ backend/apps/profiles/tests.py
@@ -6,47 +6,70 @@ from .models import Profile
 # Create your tests here.
 class ProfilesModelTests(TestCase):
 
-    def test_default_profile_creation(self):
-        """ Test that a default unverified profile can be created """
+    def setUp(self):
         # Create a new test user and profile
-        user = User.objects.create_user(username="testuser", password="Test1234!")
-        profile = Profile.objects.create(user=user)
+        self.user = User.objects.create_user(username="testuser", password="Test1234!", email="test.user@mail.utoronto.ca")
+        self.profile = Profile.objects.create(user=self.user)
 
+    def test_profile_creation(self):
+        """ Test that a default unverified profile can be created with the correct fields."""
         # Check for default values
-        self.assertEqual(profile.user, user)
-        self.assertFalse(profile.is_verified)
-        self.assertIsNone(profile.bio)
-        self.assertIsNone(profile.verification_code)
-        self.assertEqual(profile.image.name, "default/default-user.jpg")
-        print("Test: Default Profile Creation -- PASS")
+        self.assertIsInstance(self.profile, Profile)
+        self.assertEqual(self.profile.user, self.user)
+        self.assertFalse(self.profile.is_verified)
+        self.assertEqual(self.profile.bio, None)
+        self.assertEqual(self.profile.verification_code, None)
+        self.assertEqual(self.profile.profilePic, None)
+        print("Test: Default Profile Creation - PASS")
     
 
     def test_profile_str_method(self):
         """ Test that the str method returns the email of the profile passed into it """
-        # Create a user with an email
-        user = User.objects.create_user(username="testuser", password="Test1234!", email="test.user@mail.utoronto.ca")
-        profile = Profile.objects.create(user=user)
-
         # Verify __str__ returns the user's email
-        self.assertEqual(str(profile), user.email)
-        print("Test: Profile __str__ Method -- PASS")
+        self.assertEqual(str(self.profile), self.user.email)
+        print("Test: Profile __str__ Method - PASS")
 
     
     def test_generate_verification_code(self):
         """ Test that the generate verification code function works as expected """
-        # Create a user and profile, then generate a verification code
-        user = User.objects.create_user(username="testuser", password="testpass")
-        profile = Profile.objects.create(user=user)
-        code = profile.generate_verification_code()
+        # Generate a verification code
+        code = self.profile.generate_verification_code()
 
         # Check code properties - within range and is numerical
         self.assertEqual(len(code), 6)
         self.assertTrue(code.isdigit())
-        self.assertEqual(profile.verification_code, code)
-        print("Test: Profile Generate Verification Code Method -- PASS")
+        self.assertEqual(self.profile.verification_code, code)
+        print("Test: Profile Generate Verification Code Method - PASS")
 
     
+    def test_generate_new_verification_code(self):
+        """ Test to ensure a new verification code is generated everytime"""
+        old_code = self.profile.verification_code
+        new_code = self.profile.generate_verification_code()
+        self.assertNotEqual(old_code, new_code)  # ensure the codes are different
+        self.assertEqual(self.profile.verification_code, new_code)  # ensure the code has been updated
+        print("Test: Generate New Verification Code - PASS")
 
     
+    def test_default_verification_status(self):
+        """ Test to ensure the is verified is False by default"""
+        self.assertFalse(self.profile.is_verified)
+        print("Test: Defaul Verification Status - PASS")
+
+
+    def test_updated_verification_status(self):
+        """ Test to ensure verification status is verified for an user after an update""" 
+        # update the verification status for the profile
+        self.profile.is_verified = True
+        self.profile.save()
+
+        # Check is the verification status for the user's profile is updated
+        self.assertTrue(Profile.objects.get(user=self.user).is_verified)
+        print("Test: Updated Verification Status - PASS")
+
+    
+    
+
+
 
 
