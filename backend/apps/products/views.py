7f5be09b153a3056ed49a1a334a6fc93697bdb25
amyy2 <27amys@gmail.com>
2024-11-03 16:46:05 -0500

    Add edit listing form

--- backend/apps/products/views.py
+++ backend/apps/products/views.py
@@ -49,7 +49,7 @@ class ProductAPIView(APIView):
             serializer = ProductSerializer(product)
             return Response(serializer.data)
         else:
-            products = Product.objects.select_related('user').exclude(user=current_user.user)
+            products = Product.objects.select_related('user').exclude(user=current_user.user).exclude(sold=True)
             if search_term:
                 products = products.filter(name__icontains=search_term)
 
@@ -95,11 +95,39 @@ class ProductAPIView(APIView):
 
     def put(self, request, pk=None):
         product = get_object_or_404(Product, pk=pk)
-        serializer = ProductSerializer(product, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            serializer = ProductSerializer(product, data=request.data, partial=True)
+            
+            if serializer.is_valid():
+                serializer.save(user=request.user)
+
+                if 'image' not in request.FILES:
+                    logger.debug('Product upload with no image successful')
+                    return Response(serializer.data, status=status.HTTP_201_CREATED)
+                
+                image_file = request.FILES['image']
+                filename = image_file.name.replace(" ", "_")
+            
+                s3 = boto3.client(
+                    's3',
+                    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+                    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+                    region_name=settings.AWS_S3_REGION_NAME
+                )
+                
+                image_file.seek(0)
+                s3.upload_fileobj(image_file, settings.AWS_STORAGE_BUCKET_NAME, f'images/{filename}')
+
+                logger.debug('Image upload successful')
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                logger.error(f'Serializer errors: {serializer.errors}')
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                
+        except Exception as e:
+            logger.error(f'Error during image upload: {str(e)}')
+            return Response({'error': 'Failed to upload image'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR) 
 
     def delete(self, request, pk=None):
         product = get_object_or_404(Product, pk=pk)
