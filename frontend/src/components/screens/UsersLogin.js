99a74d6eaa03f0597a16127faab9b62155493169
sandhig <sandhi.ganjoo012@gmail.com>
2024-11-05 22:25:27 -0500

    update page to use mui for consistency with error handling

--- frontend/src/components/screens/UsersLogin.js
+++ frontend/src/components/screens/UsersLogin.js
@@ -1,5 +1,5 @@
+import { Button, Link, TextField, Typography } from '@mui/material';
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import HeaderPre from "../../components/HeaderPre"
@@ -10,21 +10,65 @@ const UsersLogin = () => {
     email: '',
     password: '',
   });
-
   
   const [errorMessage, setErrorMessage] = useState('');
+  const [formErrors, setFormErrors] = useState({});
   const [successMessage, setSuccessMessage] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const navigate = useNavigate()
 
-  const handleChange = (e) => {
+  const isFormInvalid = isSubmitting || Object.values(formErrors).some(error => error) || successMessage;
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@mail\.utoronto\.ca$/;
+    return emailRegex.test(email) ? '' : 'Please enter a valid UofT email address.';
+  };
+  
+  const validatePassword = (password) => {
+    if (!password) return 'Please enter your password.';
+    else if (password.length < 6) return 'Password must be at least 6 characters long.'
+    else return '';
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormErrors({ ...formErrors, [name]: '' });
+
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value,
+      [name]: value
     });
+  };
+
+  const handleBlur = (event) => {
+    const { name, value } = event.target;
+    let errorMessage = '';
+
+    if (name === 'email') {
+      errorMessage = validateEmail(value);
+    } else if (name === 'password') {
+      errorMessage = validatePassword(value);
+    }
+
+    setFormErrors({ ...formErrors, [name]: errorMessage });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    const errors = {
+      email: validateEmail(formData.email),
+      password: validatePassword(formData.password),
+    };
+
+    const hasErrors = Object.values(errors).some(error => error !== '');
+    if (hasErrors) {
+      setFormErrors(errors);
+      return;
+    }
+
+    setIsSubmitting(true);
+
     try {
       const response = await axios.post('http://3.87.240.14:8000/api/profiles/login', formData, {
         headers: {
@@ -43,8 +87,33 @@ const UsersLogin = () => {
 
       }
     } catch (error) {
-      setErrorMessage('Invalid email or password.');
+      console.log(error);
+      if (error.response && error.response.data) {
+        const backendErrors = error.response.data;
+        const fieldErrors = {};
+  
+        // generic errors
+        if (backendErrors.non_field_errors) {
+          setErrorMessage(backendErrors.non_field_errors.join(' '));
+        } else {
+          setErrorMessage('');
+        }
+  
+        // form field specific errors
+        Object.keys(backendErrors).forEach(key => {
+          if (key !== 'non_field_errors') {
+            fieldErrors[key] = backendErrors[key].join(' '); // if multiple messages for a field
+          }
+        });
+        setFormErrors(fieldErrors);
+      } else {
+        // in case of unexpected errors
+        setErrorMessage('An unexpected error occurred. Please try again.');
+      }
+
       setSuccessMessage('');
+    } finally {
+      setIsSubmitting(false);
     }
   };
 
@@ -52,36 +121,60 @@ const UsersLogin = () => {
     <div className="signup-page">
       <HeaderPre />
       <div className='signup-container'>
-        <h2>Login</h2>
-        <form onSubmit={handleSubmit} className="signup-form">
-          <div>
-            <label>UofT Email: </label>
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div>
-            <label>Password: </label>
-            <input
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <button type="submit">Login</button>
+        <h1>Login</h1>
+        <form noValidate onSubmit={handleSubmit} className="signup-form">
+          <TextField
+            label="UofT Email"
+            name="email"
+            type="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            onBlur={handleBlur}
+            variant="outlined"
+            required
+            error={!!formErrors.email}
+            helperText={formErrors.email}
+          />
+          <TextField
+            label="Password"
+            name="password"
+            type="password"
+            value={formData.password}
+            onChange={handleInputChange}
+            onBlur={handleBlur}
+            variant="outlined"
+            required
+            error={!!formErrors.password}
+            helperText={formErrors.password}
+          />
+          <Button
+            name="login"
+            type="submit"
+            variant="contained"
+            color="primary"
+            disabled={isFormInvalid}
+          >
+            {isSubmitting || successMessage ? 'Logging In...' : 'Login'}
+          </Button>
         </form>
-        <p>
-          Not a User?
-          <Link to="/profiles/signup"> Signup</Link>
-        </p>
-        {errorMessage && <p className="error-message">{errorMessage}</p>}
-        {successMessage && <p className="success-message">{successMessage}</p>}
+        <div className='side-by-side'>
+          <div className='typography'>
+            <Typography>Not a User?</Typography>
+          </div>
+          <div className='top-padding'>
+            <Link color="primary" to="/profiles/reset">Signup</Link>
+          </div>
+        </div>
+        {errorMessage && (
+          <Typography variant="body1" className="error-message">
+            {errorMessage}
+          </Typography>
+        )}
+        {successMessage && (
+          <Typography variant="body1" className="success-message">
+            {successMessage}
+          </Typography>
+        )}
       </div>
     </div>
   );
