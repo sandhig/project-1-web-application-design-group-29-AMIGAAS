5df1e18e84d60e933a742cc0fb51ef9af6211d65
ria147 <62183694+ria147@users.noreply.github.com>
2024-11-08 18:43:16 -0500

    Change welcome page buttons to mui button, add navigation to index to HeaderPre logo

--- frontend/src/components/HeaderPre.js
+++ frontend/src/components/HeaderPre.js
@@ -1,16 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Header.css'
 
 function HeaderPre() {
-    
-    return (
-      <div className="header">
-        <div className="logo-container">
-            <img className="header-logo" src="/images/header-logo-navy-cyan-white.png" alt="Website logo"></img>
-            <div className="site-title">TOO GOOD TO THROW</div>
-        </div>
+  const navigate = useNavigate();
+
+  const handleWelcome = () => {
+    navigate('/'); //Redirects to the index (home) page
+  }
+
+  return (
+    <div className="header">
+      <div className="logo-container"
+        id="logo" 
+        onClick={handleWelcome} 
+        style={{
+            cursor: 'pointer', // Changes cursor to indicate it's clickable
+        }}
+      >
+          <img className="header-logo" src="/images/header-logo-navy-cyan-white.png" alt="Website logo"></img>
+          <div className="site-title">TOO GOOD TO THROW</div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default HeaderPre;
+
