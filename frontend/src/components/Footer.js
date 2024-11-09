e71f764dc96b06080530925773b775833c5da61e
tasfia8 <tasfia8@yahoo.ca>
2024-11-09 03:22:38 -0500

    Add About Us and FAQ buttons in Footer Using mui

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Footer.css'
+import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 function Footer() {
 
--- frontend/src/components/screens/About.js
+++ frontend/src/components/screens/About.js
@@ -4,7 +4,7 @@ function About() {
     return (
         <div style={{ padding: '20px' }}>
             <h1>About Us</h1>
-            <p>We are a team of 7 University of Toronto Engineering students who developed "Too Good To Throw" as a platform to help students buy and sell second-hand items affordably and sustainably.</p>
+            <p>We are AMIGAAS, a team of 7 University of Toronto Engineering students who developed "Too Good to Throw" as a platform to help students buy and sell second-hand items affordably and sustainably.</p>
             {/* Placeholder for adding team members' photos and names in the future */}
         </div>
     );
