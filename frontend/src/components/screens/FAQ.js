28c8114fad369974a433a887679963e85da34ca6
sarahagib <sarahagib@gmail.com>
2024-11-09 07:42:56 -0500

    added header to About and FAQ pages.

--- frontend/src/components/screens/FAQ.js
+++ frontend/src/components/screens/FAQ.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import Header from "../../components/Header"
 
 function FAQ() {
     return (
-        <div style={{ padding: '20px' }}>
-            <h1>Frequently Asked Questions (FAQ)</h1>
-            <h2>Shopping on Too Good To Throw</h2>
-            <p>Q: How do I buy items on this platform?</p>
-            <p>A: Simply browse listings and contact the seller for further details.</p>
-            {/* Add more FAQs related to sign-up, login, purchasing, etc. */}
+        <div>
+            <Header/>
+            <div style={{ padding: '20px' }}>
+                <h1>Frequently Asked Questions (FAQ)</h1>
+                <h2>Shopping on Too Good To Throw</h2>
+                <p>Q: How do I buy items on this platform?</p>
+                <p>A: Simply browse listings and contact the seller for further details.</p>
+                {/* Add more FAQs related to sign-up, login, purchasing, etc. */}
+            </div>
         </div>
     );
 }
