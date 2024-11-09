aa152866d39124344d296ea18b9f97b52ab6e0dc
tasfia8 <tasfia8@yahoo.ca>
2024-11-09 03:15:00 -0500

    Continuation from epic6v1 branch commits -Added About.js

---
+++ frontend/src/components/screens/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div style={{ padding: '20px' }}>
+            <h1>About Us</h1>
+            <p>We are a team of 7 University of Toronto Engineering students who developed "Too Good To Throw" as a platform to help students buy and sell second-hand items affordably and sustainably.</p>
+            {/* Placeholder for adding team members' photos and names in the future */}
+        </div>
+    );
+}
+
+export default About;
