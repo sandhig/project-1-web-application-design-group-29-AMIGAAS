44557217b50bd44ab348fd5aaad01c333131b25a
sandhig <sandhi.ganjoo012@gmail.com>
2024-11-10 20:22:52 -0500

    move file to correct location

--- frontend/src/components/screens/profiles/EditProfile.js
+++ frontend/src/components/screens/profiles/EditProfile.js
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useUser } from '../../../context/UserContext';
+import { Button, TextField, Typography } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+
 import './EditProfile.css';
 import Header from "../../Header"
+import { useUser } from '../../../context/UserContext';
 
 function EditProfile() {
-    
     const navigate = useNavigate();
 
     const [profile, setProfile] = useState({
@@ -15,11 +16,14 @@ function EditProfile() {
         profilePic: null
     });
 
-    const [profilePicPreview, setProfilePicPreview] = useState(null);
     const { currentUser } = useUser();
     const token = localStorage.getItem('authToken');
-
+    const [formErrors, setFormErrors] = useState({});
     const [successMessage, setSuccessMessage] = useState('');
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
+    const isFormInvalid = isSubmitting || Object.values(formErrors).some(error => error);
 
     useEffect(() => {
         // Fetch profile data to populate form fields
@@ -39,31 +43,77 @@ function EditProfile() {
                     bio: data.bio || '',
                     profilePic: data.profilePic || ''
                 });
-                setProfilePicPreview(data.profilePic)
+                setSelectedImage(data.profilePic);
             })
             .catch(error => console.error('Error fetching profile:', error));
         }
     }, [currentUser, token]);
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setProfile(prevProfile => ({
-            ...prevProfile,
-            [name]: value,
-        }));
+    const validateImageType = (file) => {
+        if (file && !file.type.includes('jpeg') && !file.name.endsWith('.jpg')) {
+            return 'Please upload a .jpg or .jpeg file';
+        } else {
+            return '';
+        }
     };
 
-    const handleFileChange = (e) => {
-        const file = e.target.files[0];
-        setProfile(prevProfile => ({
-            ...prevProfile,
-            profilePic: file,
-        }));
-        setProfilePicPreview(URL.createObjectURL(file));  // Preview the selected image
+    const handleInputChange = (event) => {
+        const { name, value, type, files } = event.target;
+        setFormErrors({ ...formErrors, [name]: '' });
+
+        if (type === 'file') {
+            const file = files[0];
+
+            if (!file) {
+                return;
+            }
+
+            // Check if the file type is JPEG
+            const wrongFileType = validateImageType(file);
+            if (wrongFileType) {
+                setFormErrors({
+                    ...formErrors,
+                    image: wrongFileType
+                });
+    
+                // Reset the profile pic to null if there's an error
+                setProfile({
+                    ...profile,
+                    profilePic: null
+                });
+                return;
+            }
+
+            setFormErrors({...formErrors, image: ''});
+            setProfile({ ...profile, image: file });
+            setSelectedImage(URL.createObjectURL(file));
+
+        } else {
+            setProfile({
+                ...profile,
+                [name]: value
+                });
+        }
     };
 
     const handleSave = (e) => {
         e.preventDefault();
+
+        const newProfilePic = profile.profilePic && typeof profile.profilePic !== 'string';
+
+        if (newProfilePic) {
+            const errors = {
+                image: validateImageType(profile.profilePic),
+            };
+        
+            const hasErrors = Object.values(errors).some(error => error !== '');
+            if (hasErrors) {
+                setFormErrors(errors);
+                return;
+            }
+        }
+      
+        setIsSubmitting(true);
 
         // Structure data to match what the serializer expects
         const formData = new FormData();
@@ -72,18 +122,9 @@ function EditProfile() {
         formData.append('bio', profile.bio); 
         
         // Only add profile pic to form if it's a new file
-        if (profile.profilePic && typeof profile.profilePic !== 'string') {
+        if (newProfilePic) {
             formData.append('profilePic', profile.profilePic);
         }
-    
-        /*const updatedProfile = {
-            user: {
-                first_name: profile.first_name,
-                last_name: profile.last_name,
-            },
-            bio: profile.bio,
-            profilePic: profile.profilePic,
-        };*/
 
         fetch(`http://3.87.240.14:8000/api/profiles/edit-profile/`, {
             method: 'POST',
@@ -101,7 +142,8 @@ function EditProfile() {
                 console.error('Error saving profile:', response);
             }
         })
-        .catch(error => console.error('Error:', error));
+        .catch(error => console.error('Error:', error))
+        .finally(() => setIsSubmitting(false));
     };
 
     return (
@@ -109,54 +151,78 @@ function EditProfile() {
             <Header />
             <div className="edit-profile-container">
                 <h1>Edit Profile</h1>
-                <form onSubmit={handleSave} className='profile-form'>
-                    <label className="profile-label">
-                        First Name:
-                        <input
-                            type="text"
-                            name="first_name"
-                            value={profile.first_name}
-                            onChange={handleChange}
-                            className="profile-input"
+                <form noValidate onSubmit={handleSave} className='profile-form'>
+                    <div className="profile-photo-container">
+                        {selectedImage ? (
+                            <img className="profile-photo" src={selectedImage} alt="Profile Photo"></img>
+                        ) : (
+                            <img src="/profile-icon.jpg" alt="Default Profile" className="profile-photo" />
+                        )}
+                        <input 
+                            id="imageInput"
+                            type="file" 
+                            accept="image/jpg, image/jpeg"  
+                            onChange={handleInputChange} 
+                            style={{ display: 'none' }}
                         />
-                    </label>
-                    <label className="profile-label">
-                        Last Name:
-                        <input
-                            type="text"
-                            name="last_name"
-                            value={profile.last_name}
-                            onChange={handleChange}
-                            className="profile-input"
-                        />
-                    </label>
-                    <label className='profile-label'>
-                        Bio:
-                        <textarea
-                            name="bio"
-                            value={profile.bio}
-                            onChange={handleChange}
-                            className='profile-textarea'
-                            row="5"
-                        />
-                    </label>
-                    <label>
-                        Profile Photo:
-                        <input
-                            type="file"
-                            onChange={handleFileChange}  // Update file input handler
-                            className="profile-photo-input"
-                            accept="image/*"
-                        />
-                    </label>
-
-                    {profilePicPreview && (
-                        <img src={profilePicPreview} 
-                        alt="Profile Preview" 
-                        style={{ width: '100px', height: '100px', borderRadius: '50%' }} 
-                        />
-                    )}
-                    <button type="submit" className="profile-save-button">Save Changes</button>
+                        <label htmlFor="imageInput">
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                component="span"
+                                disabled={isSubmitting}
+                                sx={{
+                                    '&:hover': {
+                                    backgroundColor: '#007fa3',       // Custom hover color
+                                    },
+                                }}
+                            >
+                                Choose Image
+                            </Button>
+                        </label>
+                        <Typography variant="caption" color="error">{formErrors.image}</Typography>
+                    </div>
+                    <TextField
+                        label="First Name"
+                        name="first_name"
+                        type="text"
+                        value={profile.first_name}
+                        variant="outlined"
+                        disabled
+                        sx={{ input: { cursor: 'not-allowed' } }}
+                    />
+                    <TextField
+                        label="Last Name"
+                        name="last_name"
+                        type="text"
+                        value={profile.last_name}
+                        variant="outlined"
+                        disabled
+                        sx={{ input: { cursor: 'not-allowed' } }}
+                    />
+                    <TextField
+                        label="Bio"
+                        name="bio"
+                        value={profile.bio}
+                        onChange={handleInputChange}
+                        multiline
+                        rows={4}
+                        variant="outlined"
+                    />
+                    <Button
+                        name="submit"
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        disabled={isFormInvalid}
+                        sx={{
+                            '&:hover': {
+                            backgroundColor: '#007fa3',       // Custom hover color
+                            },
+                          }}
+                    >
+                        {isSubmitting ? 'Saving Changes...' : 'Save Changes'}
+                    </Button>
                     {successMessage && <p className="success-message">{successMessage}</p>}
                 </form>
             </div>
