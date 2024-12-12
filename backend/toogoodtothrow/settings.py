8bc4fce89fe66b5c8ed09e46b4470399825fd0a5
Amy Saranchuk <60277985+amyy2@users.noreply.github.com>
2024-12-12 18:02:25 -0500

    Update settings.py

--- backend/toogoodtothrow/settings.py
+++ backend/toogoodtothrow/settings.py
@@ -22,9 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m_wv1jcga9smxirv_)h%b7$ts&$#96$a+1omwn_=lq$4l4g5hr'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
